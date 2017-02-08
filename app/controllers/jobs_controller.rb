d7f5be6787afbebb12e98f722992df3a9d29d87d
mlf <micafinn@gmail.com>
2017-02-08 21:17:44 +0200

    Add full update and destroy actions in jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -19,6 +19,18 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params
